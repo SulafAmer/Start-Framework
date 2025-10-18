5626f30c0171019da91334751923a72f109a6e88
SulafAmer <168731938+SulafAmer@users.noreply.github.com>
2025-10-18 18:45:46 +0300

    added margin

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,12 +6,12 @@ export default function Footer() {
 <div className={`${footerCss.firstFooterDiv} py-5`}>
     <div className='container py-5'>
         <div className='row'>
-            <div className='col-4'>
+            <div className='col-lg-4 mb-lg-0 mb-5'>
                 <h3>LOCATION</h3>
                 <p>2215 John Daniel Drive</p>
                 <p>Clark, MO 65243</p>
             </div>
-            <div className='col-4'>
+            <div className='col-lg-4 mb-lg-0 mb-5'>
                <h3>AROUND THE WEB</h3>
                <div className='d-flex justify-content-center'>
                <div className={`${footerCss.iconBorder} d-flex justify-content-center align-items-center mx-1`}>
@@ -30,7 +30,7 @@ export default function Footer() {
 
 
             </div>
-            <div className='col-4'>
+            <div className='col-lg-4 mb-lg-0 mb-2'>
                 <h3>ABOUT FREELANCER</h3>
                 <p>Freelance is a free to use, licensed Bootstrap theme created by Route</p>
             </div>
